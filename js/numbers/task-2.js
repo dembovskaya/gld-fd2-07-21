05517520463d3f230bd357f144361e2f37a2a656
P D <p.dembovskaya@gmail.com>
2021-02-08 01:44:03 +0300

    feat: numbers tasks 1-6

---
+++ js/numbers/task-2.js
@@ -0,0 +1,11 @@
+// Найдите квадратный корень из 999. Результат округлите до целых, до десятых, до сотых.
+
+let a = 999;
+let b = Math.sqrt(a);
+
+console.log(b);
+
+console.log(b.toFixed(0));
+console.log(b.toFixed(1));
+console.log(b.toFixed(2));
+
